00cfec92da46c4045ffecb2cbcff0f54e18bc66b
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-10-23 09:45:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*number */
+
+int a = 6;
+int b = 3;
+
+a = a+ b;//6+3=9;
+b = a - b;//a=9;b=6;
+a = a - b;//9-6=3
+
+Console.WriteLine(a);
+Console.WriteLine(b);   
+
+
+
+
+
+Console.WriteLine(a*b);
+
